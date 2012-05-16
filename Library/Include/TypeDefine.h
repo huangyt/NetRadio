19ed79c88f56856f4ed88e83b1c55ad054c486e1
letion <letion@sohu.com>
2012-05-16 09:26:40 +0800

    增加类型定义及目录结构调整

---
+++ Library/Include/TypeDefine.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+* Copyright (c) 2012, Letion
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the University of California, Berkeley nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+///============================================================================
+/// \file    : TypeDeff.h
+/// \brief   : 通用类型定义头文件
+/// \author  : letion
+/// \version : 1.0
+/// \date    : 2012-05-16
+///============================================================================
+
+#ifndef __TYPE_DEFF_H__
+#define __TYPE_DEFF_H__
+
+// inttypes.h
+#ifdef _WIN32
+#include "inttypes.h"
+#else
+#include <inttypes.h>
+#endif //_WIN32
+
+#endif //__TYPE_DEFF_H__
